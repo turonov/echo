65ef7085775e31d225d2fd10c3ba902a2629a567
djumanov <diyorbekjumanov1120@gmail.com>
2023-02-14 12:04:28 +0500

    set webhook

--- app.py
+++ app.py
@@ -12,8 +12,18 @@ TOKEN = os.environ['TOKEN']
 @app.route('/webhook', methods=['POST'])
 def webhook():
     # get data from request
-    data = request.get_json(force=True)
+    update = request.get_json(force=True)
 
-    print(data)
+    # get chat_id, text from update
+    chat_id = update['message']['from']['id']
+    text = update['message']['text']
+
+    # sendMessage
+    payload = {
+        'chat_id': chat_id,
+        'text': text
+    }
+
+    r = requests.get(f'https://api.telegram.org/bot{TOKEN}/sendMessage', params=payload)
 
     return 'ok'
