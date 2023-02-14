f3b6c622861d777f9d832b8d8cc9987e232dd836
djumanov <diyorbekjumanov1120@gmail.com>
2023-02-14 10:55:23 +0500

    init

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+import requests
+import os
+
+# flask app
+app = Flask(__name__)
+
+# TOKEN
+TOKEN = os.environ['TOKEN']
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    # get data from request
+    data = print(request.get_json(force=True))
+
+    print(data)
+
+    return 'ok'
